cd54e2a2de215191b469456b8580a41dbdfc452f
code-guerilla <125157570+code-guerilla@users.noreply.github.com>
2025-11-14 10:36:02 +0100

    Remove unused configuration options
    Remove sleep and experimental features configurations

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-CONFIG_ZMK_SLEEP=y
-CONFIG_ZMK_BLE_EXPERIMENTAL_FEATURES=y
-CONFIG_ZMK_PM_SOFT_OFF=y
-CONFIG_ZMK_POINTING=y
