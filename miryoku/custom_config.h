6a6b8af61659b0835db7e6af45fd12dec6fedcb4
code-guerilla <125157570+code-guerilla@users.noreply.github.com>
2025-11-30 15:55:13 +0100

    Update copyright and clean up custom_config.h
    Updated copyright year and removed obsolete comments and macros.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,7 @@
-// Copyright 2021 Manna Harbour
+ // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// 1. DEFINE YOUR CUSTOM MACROS
+// Define your macros
 / {
     macros {
         SA_C: select_all_copy {
@@ -14,22 +14,5 @@
     };
 };
 
-// 2. DEFINE HELPER
-#define XXX &none
-
-// 3. OVERRIDE THE MAPPING
-// We accept the standard Miryoku names (LT0, LT1...) as inputs.
-// We arrange them visually to match the Anywhy Flake physical layout.
-// We insert &SA_C and other custom keys into the extra slots.
-
-#define MIRYOKU_MAPPING( \
-     LT0, LT1, LT2, LT3, LT4,           RT0, RT1, RT2, RT3, RT4, \
-     LM0, LM1, LM2, LM3, LM4,           RM0, RM1, RM2, RM3, RM4, \
-     LB0, LB1, LB2, LB3, LB4,           RB0, RB1, RB2, RB3, RB4, \
-     LZ,  LT,  LM,                      RM,  RT,  RZ \
-) \
-XXX    XXX  XXX  XXX  XXX  XXX          XXX  XXX  XXX  XXX  XXX  XXX \
-&gresc LT0  LT1  LT2  LT3  LT4          RT0  RT1  RT2  RT3  RT4  XXX \
-XXX    LM0  LM1  LM2  LM3  LM4          RM0  RM1  RM2  RM3  RM4  XXX \
-&SA_C  LB0  LB1  LB2  LB3  LB4          RB0  RB1  RB2  RB3  RB4  &kp LC(V) \
-       &kp RALT  LZ   LT   LM           RM   RT   RZ   &kp RALT  &kp RALT
+// You can also override keys if needed
+// #define MIRYOKU_ALPHAS_QWERTY
