9b1dcf68f8541ab64192e061a70ef6789d543a07
code-guerilla <125157570+code-guerilla@users.noreply.github.com>
2025-11-30 15:23:00 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -24,8 +24,8 @@
 XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
 &gresc K00  K01  K02  K03  K04               K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-SA_C K20  K21  K22  K23  K24                 K25  K26  K27  K28  K29  LC(V) \
-          RALT K32  K32  K33  K34       K35  K36  K37  RALT RALT
+&SA_C K20  K21  K22  K23  K24                 K25  K26  K27  K28  K29  &kp LC(V) \
+        &kp RALT K32  K32  K33  K34       K35  K36  K37  &kp RALT &kp RALT
 
 #endif
 
