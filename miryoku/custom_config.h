93233ebbdff206ce091b04b63a797dcf2cb2731e
code-guerilla <125157570+code-guerilla@users.noreply.github.com>
2025-11-30 16:04:28 +0100

    Delete select_all_copy macro from custom_config.h
    Removed macro definition for select_all_copy.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,19 +1,6 @@
  // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// Define your macros
-/ {
-    macros {
-        SA_C: select_all_copy {
-            compatible = "zmk,behavior-macro";
-            #binding-cells = <0>;
-            wait-ms = <40>;
-            tap-ms = <40>;
-            bindings = <&kp LC(A) &kp LC(C)>;
-        };
-    };
-};
-
 // You can also override keys if needed
 // #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_CLIPBOARD_WIN
