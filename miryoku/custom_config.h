58a809030c8872cf7761e15a766fc71e3c078871
code-guerilla <125157570+code-guerilla@users.noreply.github.com>
2025-11-30 16:18:36 +0100

    Clean up comments in custom_config.h
    Removed comments about overriding keys in custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,2 @@
  // Copyright 2022 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-// You can also override keys if needed
-// #define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_CLIPBOARD_WIN
