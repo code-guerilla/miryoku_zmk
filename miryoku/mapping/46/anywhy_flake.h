851e16f233326f7327e2800c386f0886366a9df3
Park Marcel <pam2nu@bosch.com>
2026-01-09 09:32:29 +0100

    Refactor workflow and keyboard configuration: remove obsolete flake workflow, update custom_config and kconfig, and add new outboard module for anywhy_flake.

--- miryoku/mapping/46/anywhy_flake.h
+++ miryoku/mapping/46/anywhy_flake.h
@@ -13,10 +13,10 @@
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
 XXX  XXX  XXX  XXX  XXX  XXX                 XXX  XXX  XXX  XXX  XXX  XXX \
-&gresc  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
-&kp TAB K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
-&u_select_all_copy  K20  K21  K22  K23  K24     K25  K26  K27  K28  K29  XXX \
-        &kp RALT  K32  K32  K33  K34       K35  K36  K37  &kp RALT  &kp RALT
+&gresc  K00  K01  K02  K03  K04              K05  K06  K07  K08  K09  XXX \
+&kp TAB K10  K11  K12  K13  K14              K15  K16  K17  K18  K19  XXX \
+&u_CA   K20  K21  K22  K23  K24              K25  K26  K27  K28  K29  XXX \
+        &kp RALT  K32  K32  K33  K34    K35  K36  K37  &kp RALT  &kp RALT
 
 #endif
 
